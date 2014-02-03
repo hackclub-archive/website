9f2a4607e6f9614a669e3ae6a01d4b246e4f436a
Zach Latta <zach@zachlatta.com>
2014-02-03 04:59:41 -0800

    Implement user registration logic with serialization to datastore.

--- hackedu/users.go
+++ hackedu/users.go
@@ -3,18 +3,39 @@ package hackedu
 import (
 	"time"
 
+	"code.google.com/p/go.crypto/scrypt"
+
 	"appengine"
 	"appengine/datastore"
 )
 
 type User struct {
-	Name        string
-	Email       string
-	Password    string
-	CreatedAt   time.Time
-	SignInCount int
+	CreatedAt time.Time
+	Name      string
+	Email     string
+	Password  []byte
 }
 
-func userKey(c appengine.Context) *datastore.Key {
-	return datastore.NewKey(c, "User", "user", 0, nil)
+func RegisterUser(c appengine.Context, name, email, password string) (*User,
+	*datastore.Key, error) {
+	hashedPassword, err := scrypt.Key([]byte(password), []byte("saltgoeshere"),
+		16384, 8, 1, 32)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	user := &User{
+		CreatedAt: time.Now(),
+		Name:      name,
+		Email:     email,
+		Password:  hashedPassword,
+	}
+
+	key, err := datastore.Put(c, datastore.NewIncompleteKey(c, "user", nil),
+		user)
+	if err != nil {
+		return user, key, err
+	}
+
+	return user, key, nil
 }
--- hackedu/users_test.go
+++ hackedu/users_test.go
@@ -1,22 +1,36 @@
 package hackedu
 
 import (
+	"bytes"
 	"testing"
 
 	"appengine/aetest"
 	"appengine/datastore"
 )
 
-func TestUserKey(t *testing.T) {
+func TestRegisterValidUser(t *testing.T) {
 	c, err := aetest.NewContext(nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer c.Close()
 
-	key := userKey(c)
+	name := "foo"
+	email := "foo@bar.com"
+	password := "foobarfoobar"
 
-	if !key.Equal(datastore.NewKey(c, "User", "user", 0, nil)) {
-		t.Errorf("Created user key does not match expected.")
+	user, key, err := RegisterUser(c, name, email, password)
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+
+	user2 := &User{}
+	if err = datastore.Get(c, key, user2); err != nil {
+		t.Errorf("Failed to retrieve user from datastore. Err: %s", err.Error())
+	}
+
+	if user.Name != user2.Name || user.Email != user2.Email ||
+		!bytes.Equal(user.Password, user2.Password) {
+		t.Errorf("Retrieved invalid user. Expected: %+v, actual %+v", user, user2)
 	}
 }
