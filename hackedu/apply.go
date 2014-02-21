51c933883248628c3ea70247b59bc7628351dea7
Zach Latta <zach@zachlatta.com>
2014-02-21 09:09:08 -0800

    Send email to me upon registration.

--- hackedu/apply.go
+++ hackedu/apply.go
@@ -2,10 +2,12 @@ package hackedu
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"appengine"
 	"appengine/datastore"
+	"appengine/mail"
 )
 
 type ApplicationForm struct {
@@ -44,6 +46,50 @@ func Apply(w http.ResponseWriter, r *http.Request) {
 	_, err = datastore.Put(c, userKey, &user)
 	if err != nil {
 		serveError(c, w, err)
+		return
+	}
+
+	msg := &mail.Message{
+		Sender:  user.FirstName + " " + user.LastName + " <" + user.Email + ">",
+		To:      []string{"zach@zachlatta.com"},
+		Subject: "hackEDU Application",
+		Body: `
+# User Information
+
+Name: ` + user.FirstName + ` ` + user.LastName + `
+Email: ` + user.Email + `
+GitHub: ` + user.GitHub + `
+Twitter: ` + user.Twitter + `
+
+# Application Information
+
+High School: ` + application.HighSchool + `
+
+## Interesting Project
+
+` + application.InterestingProject + `
+
+## System Hacked
+
+` + application.SystemHacked + `
+
+## Passion
+
+` + application.Passion + `
+
+## Story 
+
+` + application.Story + `
+
+## Why
+
+` + application.Why + `
+
+`,
+	}
+
+	if err := mail.Send(c, msg); err != nil {
+		serveError(c, w, errors.New("Could not send email"))
 		return
 	}
 
