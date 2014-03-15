30f1fa277ee6db40bb5c307230b09803dc80f2e9
Zach Latta <zach@zachlatta.com>
2014-03-14 22:34:54 -0700

    Hardcode email responses on user registration.

--- v1/route/users.go
+++ v1/route/users.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/coopernurse/gorp"
 	"github.com/hackedu/backend/v1/model"
+	"github.com/hackedu/backend/v1/service/mail"
 )
 
 func AddUser(user model.User, db gorp.SqlExecutor) (int, string) {
@@ -38,7 +40,71 @@ func AddUser(user model.User, db gorp.SqlExecutor) (int, string) {
 
 	db.Insert(application)
 
-	// TODO: Send emails
+	msg := &mail.Message{
+		Sender:  user.FirstName + " " + user.LastName + " <" + user.Email + ">",
+		To:      []string{"Zach Latta <zach@zachlatta.com>"},
+		Subject: "hackEDU Application",
+		Body: `# User Information
+
+Name: ` + user.FirstName + ` ` + user.LastName + `
+Email: ` + user.Email + `
+GitHub: ` + user.GitHub + `
+Twitter: ` + user.Twitter + `
+
+# Application
+
+## High School
+
+` + application.HighSchool + `
+
+## Interesting Project
+
+` + application.InterestingProject + `
+
+## System Hacked
+
+` + application.SystemHacked + `
+
+## Passion
+
+` + application.Passion + `
+
+## Story 
+
+` + application.Story + `
+
+## Why
+
+` + application.Why + `
+
+`,
+	}
+
+	if err := mail.Send(msg); err != nil {
+		log.Println(err)
+		return http.StatusInternalServerError, "Could not send email"
+	}
+
+	msg = &mail.Message{
+		Sender: "Zach Latta <zach@zachlatta.com>",
+		To: []string{
+			fmt.Sprintf("%s %s <%s>", user.FirstName, user.LastName, user.Email),
+		},
+		Subject: "hackEDU Application",
+		Body: `Hey ` + user.FirstName + `!
+
+Thanks for applying for hackEDU. We've received your application and you can
+expect to hear from us shortly. If you have any questions, please don't
+hesitate to email me at zach@zachlatta.com.
+
+Best regards,
+Zach Latta
+`,
+	}
+
+	if err := mail.Send(msg); err != nil {
+		return http.StatusInternalServerError, "Could not send email"
+	}
 
 	json, err := json.Marshal(user)
 	if err != nil {
