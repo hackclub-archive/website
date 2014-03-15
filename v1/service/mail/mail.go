c504d02cd88ad3a31025650a9ba77bdea7d3f0a3
Zach Latta <zach@zachlatta.com>
2014-03-14 22:09:46 -0700

    Add subject field to emails and use a template.

--- v1/service/mail/mail.go
+++ v1/service/mail/mail.go
@@ -2,8 +2,12 @@
 package mail
 
 import (
+	"bytes"
+	"log"
 	"net/smtp"
-	"os"
+	"text/template"
+
+	"github.com/hackedu/backend/v1/helper"
 )
 
 var auth smtp.Auth
@@ -11,33 +15,48 @@ var auth smtp.Auth
 func init() {
 	auth = smtp.PlainAuth(
 		"",
-		os.Getenv("SMTP_USERNAME"),
-		os.Getenv("SMTP_PASSWORD"),
-		os.Getenv("SMTP_HOST"),
+		helper.GetConfig("SMTP_USERNAME"),
+		helper.GetConfig("SMTP_PASSWORD"),
+		helper.GetConfig("SMTP_HOST"),
 	)
 }
 
 type Message struct {
-	Sender  string // required
-	ReplyTo string // may be empty
-
-	// At least one of these slices must have a non-zero length, except when
-	// calling SendToAadmins.
-	To, Cc, Bcc []string
-
-	// At least one of Body or HTMLBody must by non-empty.
-	Body     string
-	HTMLBody string
+	Sender  string
+	To      []string
+	Subject string
+	Body    string
 }
+
+const emailTemplate = `From: {{.Sender}}
+To: {{range .To}}{{.}},{{end}}
+Subject: {{.Subject}}
+
+{{.Body}}
+`
 
 // Send sends an email message.
 func Send(msg *Message) error {
-	err := smtp.SendMail(
-		os.Getenv("SMTP_HOST")+":"+os.Getenv("SMTP_PORT"),
+	var doc bytes.Buffer
+
+	t := template.New("emailTemplate")
+	t, err := t.Parse(emailTemplate)
+	if err != nil {
+		log.Println("Error parsing mail template", err)
+		return err
+	}
+	err = t.Execute(&doc, msg)
+	if err != nil {
+		log.Println("Error executing mail template", err)
+		return err
+	}
+
+	err = smtp.SendMail(
+		helper.GetConfig("SMTP_HOST")+":"+helper.GetConfig("SMTP_PORT"),
 		auth,
 		msg.Sender,
 		msg.To,
-		[]byte(msg.Body),
+		doc.Bytes(),
 	)
 
 	if err != nil {
