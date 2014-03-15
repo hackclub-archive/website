8fb49dc9153c4593b739a5950ae2bc8cbddc758f
Zach Latta <zach@zachlatta.com>
2014-03-15 14:54:33 -0700

    Use Martini CORS service instead of our own.

--- v1/v1.go
+++ v1/v1.go
@@ -1,19 +1,32 @@
 package v1
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/codegangsta/martini"
 	"github.com/coopernurse/gorp"
 	"github.com/hackedu/backend/v1/model"
 	"github.com/hackedu/backend/v1/route"
 	"github.com/martini-contrib/binding"
+	"github.com/zachlatta/cors"
 )
 
 func Setup(m *martini.ClassicMartini) {
 	// TODO: Only apply middleware on /v1/** routes
-	m.Use(allowCORS)
+	m.Use(cors.Allow(&cors.Options{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST"},
+		MaxAge:          5 * time.Minute,
+	}))
 	m.MapTo(Dbm, (*gorp.SqlExecutor)(nil))
 
 	m.Get("/v1/schools", route.GetSchools)
 
 	m.Post("/v1/users", binding.Bind(model.User{}), route.AddUser)
+
+	// OPTIONS catchall for CORS.
+	m.Options("/**", func() int {
+		return http.StatusOK
+	})
 }
