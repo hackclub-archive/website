7910a1bd0f641131a79e6aebc99ef82d1f96e0cb
Zach Latta <zach@zachlatta.com>
2014-04-24 14:53:42 -0700

    Remove old middleware file.

--- middleware.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "net/http"
-
-func allowCORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept")
-}
