3f0b68e9024f7b0d6d05dc3f5409e0167507e96f
Zach Latta <zach@zachlatta.com>
2014-06-12 19:34:18 -0700

    Return more information with tokens.

--- model/token.go
+++ model/token.go
@@ -8,14 +8,19 @@ import (
 
 // Token represents a JSON Web Token (JWT) as returned to the user.
 type Token struct {
-	Body string `json:"token"`
+	UserID   int64     `json:"id"`
+	UserType string    `json:"type"`
+	Token    string    `json:"token"`
+	Expires  time.Time `json:"expires"`
 }
 
 // NewToken creates a new Token from a provided user.
 func NewToken(user *User) (*Token, error) {
+	expires := time.Now().Add(time.Hour * 72)
+
 	token := jwt.New(jwt.GetSigningMethod("HS256"))
 	token.Claims["id"] = user.ID
-	token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+	token.Claims["exp"] = expires.Unix()
 
 	// TODO: Sign the token with an actual secret
 	tokenString, err := token.SignedString([]byte("secret"))
@@ -23,5 +28,6 @@ func NewToken(user *User) (*Token, error) {
 		return nil, err
 	}
 
-	return &Token{tokenString}, nil
+	return &Token{UserID: user.ID, UserType: user.Type, Token: tokenString,
+		Expires: expires}, nil
 }
