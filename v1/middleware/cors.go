2bebb9cd08a4b97506ebfa931946c8c0f74139fa
Zach Latta <zach@zachlatta.com>
2014-09-29 13:11:48 -0700

    Use CORS middleware with Negroni.

--- v1/middleware/cors.go
+++ v1/middleware/cors.go
@@ -2,16 +2,14 @@ package middleware
 
 import "net/http"
 
-type CORS struct{}
-
-func (c *CORS) Handle(w http.ResponseWriter, r *http.Request) bool {
+func CORS(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers",
 		"Content-Type, Authorization")
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
-		return false
+		return
 	}
 
-	return true
+	next(w, r)
 }
