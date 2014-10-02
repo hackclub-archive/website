744573bd8fb62f7f05555ad67e0becaeea7e3f4a
Zach Latta <zach@zachlatta.com>
2014-10-02 12:21:13 -0700

    Create attributions page.

--- hackedu.rb
+++ hackedu.rb
@@ -68,4 +68,28 @@ class HackEDU < Sinatra::Base
   get '/contact' do
     erb :contact
   end
+
+  get '/attributions' do
+    icons = [
+      {
+        name: 'Books',
+        url: 'http://thenounproject.com/term/books/21509/',
+        author: 'Piotrek Chuchla',
+        author_url: 'http://www.piotrekchuchla.com'
+      },
+      {
+        name: 'Community',
+        url: 'http://thenounproject.com/term/community/5040/',
+        author: 'Dmitry Baranovskiy',
+        author_url: 'http://dmitry.baranovskiy.com'
+      },
+      {
+        name: 'Open Source',
+        url: 'http://thenounproject.com/term/open-source/8233/',
+        author: 'Oriol Carbonell',
+        author_url: 'http://www.hiperic.com'
+      }
+    ]
+    erb :attributions, locals: { icons: icons }
+  end
 end
