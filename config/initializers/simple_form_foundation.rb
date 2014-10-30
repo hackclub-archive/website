9f9e066744113ec45b9b9f8f75d530684ebf3aa2
Zach Latta <zach@zachlatta.com>
2014-10-30 12:31:13 -0700

    Set up simple_form.

---
+++ config/initializers/simple_form_foundation.rb
@@ -0,0 +1,23 @@
+# Use this setup block to configure all options available in SimpleForm.
+SimpleForm.setup do |config|
+  config.wrappers :foundation, class: :input, hint_class: :field_with_hint, error_class: :error do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label_input
+    b.use :error, wrap_with: { tag: :small }
+    b.use :hint,  wrap_with: { tag: :span, class: :hint }
+  end
+
+  # CSS class for buttons
+  config.button_class = 'button'
+
+  # CSS class to add for error notification helper.
+  config.error_notification_class = 'alert-box alert'
+
+  # The default wrapper to be used by the FormBuilder.
+  config.default_wrapper = :foundation
+end
