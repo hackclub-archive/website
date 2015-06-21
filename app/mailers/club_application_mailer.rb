f4216d5bf0571b04701d4eddd8991ced122c3a3c
Max Wofford <max@hackedu.us>
2015-06-21 16:55:05 -0700

    Revert "Send emails from 'hackEDU Team <team@hackedu.us>'"

--- app/mailers/club_application_mailer.rb
+++ app/mailers/club_application_mailer.rb
@@ -1,5 +1,5 @@
 class ClubApplicationMailer < ActionMailer::Base
-  default from: 'hackEDU Team <team@hackedu.us>'
+  default from: 'Zach Latta <team@hackedu.us>'
 
   def applicant_confirmation(application)
     @application = application
@@ -13,7 +13,7 @@ class ClubApplicationMailer < ActionMailer::Base
     @application = application
 
     to = Mail::Address.new 'team@hackedu.us'
-    to.display_name = 'hackEDU Team'
+    to.display_name = 'Zach Latta'
     mail(to: to.format, from: @application.mail_address.format,
          subject: 'hackEDU Application')
   end
